155bef32c000a609343845d62cbc0307d479f152
VincentGe <cosplox@outlook.com>
2023-04-18 17:56:32 +0800

    package widgets index

---
+++ widgets/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) CDU
+
+"""Model Docstrings
+
+"""
+
+from __future__ import absolute_import
+from __future__ import annotations
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from .circle_widget import CircleWidget
+
+__all__ = ['CircleWidget']
