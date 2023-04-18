f88d4ffc71b4169911c6d152130c51eefe285671
VincentGe <cosplox@outlook.com>
2023-04-19 01:40:23 +0800

    init projects: install packages

---
+++ install_packages.ps1
@@ -0,0 +1,5 @@
+python -m pip install -U pip
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+pip install -U openai-whisper
+pip install -U PySide6
+pip install -U OpenCC
