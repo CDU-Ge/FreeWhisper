9f58187ce550439bfa52aa51ead72dbbe2b27079
VincentGe <cosplox@outlook.com>
2024-04-28 19:34:23 +0800

    update FreeWhisperLaunch.py

--- FreeWhisperLaunch.py
+++ FreeWhisperLaunch.py
@@ -11,11 +11,13 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
+import os.path
 import sys
 from dotenv import load_dotenv
 
 if __name__ == '__main__':
-    load_dotenv('.dev.env')
+    if os.path.exists('.dev.env'):
+        load_dotenv('.dev.env')
     from FreeWhisper.__main__ import main
 
     main(sys.argv)
